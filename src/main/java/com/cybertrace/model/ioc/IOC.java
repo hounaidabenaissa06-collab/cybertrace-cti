486693fd057f3529626ebe761541fcae2e86cecc
Hounaida Benaissa <hounaidabenaissa.06@gmail.com>
2026-05-08 15:16:43 +0100

    feat: add IOC model

---
+++ src/main/java/com/cybertrace/model/ioc/IOC.java
@@ -0,0 +1,14 @@
+package com.cybertrace.model.ioc;
+
+public class IOC {
+
+    private String value;
+
+    public IOC(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
