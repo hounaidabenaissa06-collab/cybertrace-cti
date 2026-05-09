f414bdbf7ebf3769a23adf037dc8fc94fff259bd
kaoutar995 <kaoutarnabil04@gmail.com>
2026-05-09 23:49:11 +0200

    Update Analyzable.java

--- src/main/java/com/cybertrace/interfaces/Analyzable.java
+++ src/main/java/com/cybertrace/interfaces/Analyzable.java
@@ -1,6 +1,21 @@
 package com.cybertrace.interfaces;
 
+/**
+ * Interface représentant un objet analysable dans le système CTI.
+ * Toute entité pouvant être analysée (ThreatActor, Campaign, HuntMission)
+ * doit implémenter cette interface.
+ */
 public interface Analyzable {
+
+    /**
+     * Retourne une analyse textuelle de l'entité.
+     * @return chaîne de caractères décrivant l'analyse
+     */
     String analyze();
+
+    /**
+     * Calcule et retourne le score de risque de l'entité.
+     * @return score entre 0.0 et 10.0
+     */
     double getRiskScore();
 }
