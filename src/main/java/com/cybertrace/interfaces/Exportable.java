7ce02fc9d8ff01c542321925559efb52221237ae
kaoutar995 <kaoutarnabil04@gmail.com>
2026-05-09 23:49:35 +0200

    Update Exportable.java

--- src/main/java/com/cybertrace/interfaces/Exportable.java
+++ src/main/java/com/cybertrace/interfaces/Exportable.java
@@ -1,6 +1,20 @@
 package com.cybertrace.interfaces;
 
+/**
+ * Interface pour les entités pouvant être exportées.
+ * Implémentée par Campaign et tout objet exportable en rapport.
+ */
 public interface Exportable {
+
+    /**
+     * Exporte l'entité au format CSV (une ligne).
+     * @return ligne CSV
+     */
     String exportToCSV();
+
+    /**
+     * Exporte l'entité au format texte lisible.
+     * @return représentation textuelle formatée
+     */
     String exportToTXT();
 }
