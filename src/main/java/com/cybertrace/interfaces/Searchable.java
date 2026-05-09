a68a8f2eef45dee8b0bafcde748a3d221bdd60a6
kaoutar995 <kaoutarnabil04@gmail.com>
2026-05-09 23:50:41 +0200

    Update Searchable.java

--- src/main/java/com/cybertrace/interfaces/Searchable.java
+++ src/main/java/com/cybertrace/interfaces/Searchable.java
@@ -2,7 +2,25 @@ package com.cybertrace.interfaces;
 
 import java.util.List;
 
+/**
+ * Interface générique pour les entités permettant la recherche.
+ * Le paramètre de type T représente le type d'objet retourné par les recherches.
+ * Exemple : Searchable<ThreatActor> pour rechercher des acteurs.
+ */
 public interface Searchable<T> {
+
+    /**
+     * Recherche des entités par requête textuelle générique.
+     * @param query texte de recherche
+     * @return liste des entités correspondantes
+     */
     List<T> search(String query);
+
+    /**
+     * Filtre les entités selon un champ et une valeur précis.
+     * @param field  nom du champ (ex: "origin", "type")
+     * @param value  valeur attendue du champ
+     * @return liste filtrée
+     */
     List<T> filter(String field, String value);
 }
